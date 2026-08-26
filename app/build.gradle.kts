85fe55966ccac12f335f444ad060a5e5a1422d3d
Jenil Makwana <jenilmakwana666666@gmail.com>
2026-08-26 20:51:58 +0530

    Configure Android application build settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,32 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
+android {
+    namespace = "com.itsdark.youtubegrowthai"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.itsdark.youtubegrowthai"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
