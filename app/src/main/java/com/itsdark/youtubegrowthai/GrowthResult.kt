7f26f52884ce9b301ca28abd1e73a936058e4d8a
Jenil Makwana <jenilmakwana666666@gmail.com>
2026-08-27 22:03:48 +0530

    Add GrowthResult data class for YouTube growth

---
+++ app/src/main/java/com/itsdark/youtubegrowthai/GrowthResult.kt
@@ -0,0 +1,13 @@
+package com.itsdark.youtubegrowthai
+
+data class GrowthResult(
+    val titles: String,
+    val description: String,
+    val hook: String,
+    val hashtags: String,
+    val keywords: String,
+    val thumbnailText: String,
+    val cta: String,
+    val growthTips: String,
+    val alternativeTitles: String
+)
